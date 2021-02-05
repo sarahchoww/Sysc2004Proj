712eaf639e2819e2a0a6638b54f3a826b257a718
sarahchoww <chow.sarah@hotmail.com>
2021-02-04 19:59:58 -0500

    Product class

--- Labs/proj/src/Product.java
+++ Labs/proj/src/Product.java
@@ -1,3 +1,25 @@
 public class Product {
     //Beginning of Product Class
+
+    private final String name;
+    private final int productID;
+    private final double price;
+
+    public Product(String name, int productID, double price){
+        this.name = name;
+        this.productID = productID;
+        this.price = price;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public int getProductID(){
+        return productID;
+    }
+
+    public double getPrice() {
+        return price;
+    }
 }

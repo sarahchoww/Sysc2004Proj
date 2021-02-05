b340b6ddf527f2c604155f6c86c161bc31918b21
sarahchoww <chow.sarah@hotmail.com>
2021-02-04 20:29:16 -0500

    Incomplete inventory

--- Labs/proj/src/Inventory.java
+++ Labs/proj/src/Inventory.java
@@ -1,3 +1,27 @@
+import java.util.ArrayList;
+
 public class Inventory {
     //Beginning of inventory class
+
+    private ArrayList<Integer> stock = new ArrayList<>();
+    private ArrayList<Product> products = new ArrayList<>();
+
+    /*
+    public Inventory(){
+
+    }
+
+    public int getStock(int productID){
+        return
+    }
+
+    public void setStock(int stock, String name){
+
+    }
+
+    public void removeStock(int store, int productID){
+
+    }*/
+
+
 }
